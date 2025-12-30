04f247cd3884125a15741f81b67dba1d96686d84
kilianpaquier <8428791-u.kilianpaquier@users.noreply.gitlab.com>
2025-12-30 15:56:50 +0100

    test(configure): add missing testcases
    Signed-off-by: kilianpaquier <8428791-u.kilianpaquier@users.noreply.gitlab.com>

--- pkg/generate_options_test.go
+++ pkg/generate_options_test.go
@@ -1,7 +1,37 @@
 package engine_test
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	engine "github.com/kickr-dev/engine/pkg"
+)
 
 func TestConfigure(t *testing.T) {
+	t.Run("no_options", func(t *testing.T) {
+		// Act
+		engine.Configure()
 
+		// Assert
+		assert.False(t, engine.Forced())
+		assert.NotNil(t, engine.GetLogger())
+	})
+
+	t.Run("options", func(t *testing.T) {
+		// Arrange
+		buf := strings.Builder{}
+
+		// Act
+		engine.Configure(engine.WithForce(true), engine.WithLogger(engine.NewTestLogger(&buf)))
+
+		// Assert
+		assert.True(t, engine.Forced())
+		logger := engine.GetLogger()
+		require.NotNil(t, logger)
+		logger.Infof("some text to verify")
+		assert.Equal(t, "some text to verify", buf.String())
+	})
 }
